fd143e34ac186cd27b68e805328101180e7e2d9d
LittleNiddle <rjsgk4278@gmail.com>
2025-12-03 11:19:16 +0900

    new

--- git_syntax/2.basic_command.sh
+++ git_syntax/2.basic_command.sh
@@ -29,3 +29,16 @@ git push origin 브랜치명 --force
 git checkout 커밋 아이디
 # 특정 브랜치로으 전환
 git checkout 브랜치명
+
+# pull은 원격사항을 local로 내려받는 것(fetch + merge)
+# head까지 변경
+git pull origin main
+
+# fetch는 원격사항을 lcoal로 가져오되, 병합은 하지 않는 것.
+# head는 그대로
+git fetch origin main
+
+# commitID간 또는 브랜치 사이의 비교 명령어
+git dif commitID1 commitID2
+git diff 브랜치1 브랜치2
+
